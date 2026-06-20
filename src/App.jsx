1416c71d25ef95ef226f757f7d33104feb8ec9d9
Dylan0100 <Dylanbutt0100@icloud.com>
2026-06-20 20:26:11 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Plan from './pages/Plan'
-
+import PrivacyPolicy from './pages/PrivacyPolicy'
+import Terms from './pages/Terms'
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/plan" element={<Plan />} />
+        <Route path="/privacy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<Terms />} />
       </Routes>
     </BrowserRouter>
   )
